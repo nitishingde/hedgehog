55a06ee0110ba17fa645024021528f8dff548ba5
Nitish Shingde <ngshingde@gmail.com>
2022-02-22 15:22:44 -0700

    Integrate comm in demo

--- demo.cpp
+++ demo.cpp
@@ -1,91 +1,14 @@
 #include "hedgehog/hedgehog.h"
 #include <iostream>
-
-// Class that represents a communication queue
-class CommQueue {
-    int commId_ {};
-    std::queue<char *> queue_ {};
-    std::mutex mutex_ {};
-public:
-    explicit CommQueue(int commId) : commId_(commId) {}
-    virtual ~CommQueue() = default;
-
-    char *popFront() {
-        std::lock_guard<std::mutex> lc(this->mutex_);
-        auto element = queue_.front();
-        assert(element != nullptr);
-        queue_.pop();
-        return element;
-    }
-
-    void push(char *data) {
-        std::lock_guard<std::mutex> lc(this->mutex_);
-        queue_.push(data);
-    }
-
-    bool hasElem() {
-        std::lock_guard<std::mutex> lc(this->mutex_);
-        return !queue_.empty();
-    }
-
-    [[nodiscard]] int commId() const {
-        return commId_;
-    }
-
-};
-
-// Class that represents a communicator sending/receiving data between graphs, each 'node' has its own communication queue
-class Communicator {
-//    std::unordered_map<int, std::shared_ptr<CommQueue>> commQueueMap;
-    std::vector<std::shared_ptr<CommQueue>> commQueues_;
-
-public:
-    Communicator() = default;
-    virtual ~Communicator() = default;
-
-
-    void initComm(int numNodes) {
-        commQueues_.resize(numNodes);
-        for (int id = 0; id < numNodes; ++id) {
-            auto commQueue = std::make_shared<CommQueue>(id);
-//            commQueueMap.insert({{id, commQueue}});
-            commQueues_[id] = commQueue;
-
-        }
-    }
-
-    void sendMessage(std::ostringstream const &oss, int srcId, int destId) {
-        size_t size = oss.str().size() + 1;
-//        std::cout << srcId << " sending to " << destId << " " << size << " bytes" << std::endl;
-
-        auto const &destComm = commQueues_[destId];
-        char *cstr = new char[size];
-        std::strcpy(cstr, oss.str().c_str());
-        destComm->push(cstr);
-    }
-
-    bool hasMessage(int srcId) {
-        return commQueues_[srcId]->hasElem();
-    }
-
-    std::istringstream receiveMessage(int srcId) {
-        auto &srcComm = commQueues_[srcId];
-        assert(srcComm->hasElem());
-
-        auto buffer = srcComm->popFront();
-        std::istringstream iss((std::string(buffer)));
-        return iss;
-    }
-
-};
+#include "comm.h"
 
 // A block of memory, contains information about the source and destination for the data (based on decomposition in main)
 template<class MatrixType>
-class CommMatrixBlock {
+class CommMatrixBlock: public comm::VarLabel {
     static_assert(std::is_arithmetic_v<MatrixType>, "The template parameter MatrixType should be an arithmetic value.");
     int const
+            src_,
             dest_;
-    int const src_;
 
     size_t const
             indexRow_ = 0,
@@ -97,14 +20,15 @@ class CommMatrixBlock {
             data_;
 
 public:
-    CommMatrixBlock(int const src,
+    CommMatrixBlock(std::string name,
+                    int const src,
                     int const dest,
                     size_t const indexRow,
                     size_t const indexCol,
                     size_t const height,
                     size_t const width)
-            : src_(src), dest_(dest), indexRow_(indexRow), indexCol_(indexCol), height_(height), width_(width) {
-        data_ = std::make_unique<std::vector<MatrixType>>(height_ * width_, 0);
+            : comm::VarLabel(name), src_(src), dest_(dest), indexRow_(indexRow), indexCol_(indexCol), height_(height), width_(width) {
+        data_ = std::make_unique<std::vector<MatrixType>>(height_ * width_, comm::getMpiNodeId());
     }
 
     CommMatrixBlock(CommMatrixBlock<MatrixType> &&toMove) noexcept:
@@ -112,6 +36,7 @@ public:
             indexRow_(toMove.indexRow_), indexCol_(toMove.indexRow_),
             height_(toMove.height_), width_(toMove.width_) {
         data_.swap(toMove.data_);
+        this->name_ = std::move(toMove.name_);
     }
 
     virtual ~CommMatrixBlock() = default;
@@ -134,6 +59,7 @@ public:
     }
 
     std::ostream const &serialize(std::ostream &ostream) const {
+        comm::VarLabel::serialize(ostream);
         ostream << src_ << " " << dest_ << " " << indexRow_ << " " << indexCol_ << " " << height_ << " " << width_;
         std::for_each(data().begin(), data().end(), [&ostream](MatrixType const &data) { ostream << " " << data; });
         // Use null terminated string to aid with defining length of the data
@@ -142,6 +68,7 @@ public:
     }
 
     static std::shared_ptr<CommMatrixBlock<MatrixType>> deserialize(std::istream &istream) {
+        std::string name;
         int src;
         int dst;
         size_t
@@ -150,12 +77,12 @@ public:
                 height,
                 width;
 
-        istream >> src >> dst >> indexRow >> indexCol >> height >> width;
+        istream >> name >> src >> dst >> indexRow >> indexCol >> height >> width;
 
-        auto output = std::make_shared<CommMatrixBlock<MatrixType>>(src, dst, indexRow, indexCol, height, width);
+        auto output = std::make_shared<CommMatrixBlock<MatrixType>>(name, src, dst, indexRow, indexCol, height, width);
 
         for (auto &data: output->data()) { istream >> data; }
-        return std::move(output);
+        return output;
     }
 };
 
@@ -163,15 +90,15 @@ public:
 template<class MatrixType>
 class LocalMatrixState : public hh::AbstractState<CommMatrixBlock<MatrixType>, CommMatrixBlock<MatrixType>> {
 private:
-    int id_;
+    int nodeId_;
 
 public:
-    LocalMatrixState(int id) : id_(id) {}
+    LocalMatrixState() : nodeId_(comm::getMpiNodeId()) {}
 
     ~LocalMatrixState() override = default;
 
     void execute(std::shared_ptr<CommMatrixBlock<MatrixType>> ptr) override {
-        if (ptr->dest() == id_){
+        if (ptr->dest() == nodeId_){
             this->push(ptr);
         }
     }
@@ -181,15 +108,15 @@ public:
 template<class MatrixType>
 class RemoteMatrixState : public hh::AbstractState<CommMatrixBlock<MatrixType>, CommMatrixBlock<MatrixType>> {
 private:
-    int id_;
+    int nodeId_;
 
 public:
-    RemoteMatrixState(int id) : id_(id) {}
+    RemoteMatrixState() : nodeId_(comm::getMpiNodeId()) {}
 
     ~RemoteMatrixState() override = default;
 
     void execute(std::shared_ptr<CommMatrixBlock<MatrixType>> ptr) override {
-        if (ptr->dest() != id_){
+        if (ptr->dest() != nodeId_){
             this->push(ptr);
         }
     }
@@ -204,31 +131,25 @@ private:
 
     // TODO: Determine way to receive special message that shows "done"
     size_t numberElemRcv_ = 0;
-    std::shared_ptr<Communicator> communicator_;
-    int id_;
+    int srcId_;
 
 public:
 
-    explicit CommReceiveTask(int id, std::shared_ptr<Communicator> communicator, size_t numberBlocksToReceive) :
+    explicit CommReceiveTask(int srcId, size_t numberBlocksToReceive) :
             hh::AbstractTask<CommMatrixBlock<MatrixType>, void>("Comm Receiver task", 1, true),
-            numberBlocksToReceive_(numberBlocksToReceive), id_(id), communicator_(communicator) {};
+            numberBlocksToReceive_(numberBlocksToReceive), srcId_(srcId) {};
 
     ~CommReceiveTask() override = default;
 
     void execute([[maybe_unused]]std::shared_ptr<void> ptr) override {
         using namespace std::chrono_literals;
         for (size_t rcv = 0; rcv < numberBlocksToReceive_; ++rcv) {
-
-            while (!communicator_->hasMessage(this->id_)) {
+            auto varName = "block"+std::to_string(rcv)+std::to_string(srcId_);
+            while (!comm::Communicator::hasMessage(varName, this->srcId_)) {
                 std::this_thread::sleep_for(50ms);
             }
-
-
-            std::istringstream iss = communicator_->receiveMessage(this->id_);
+            std::istringstream iss = comm::Communicator::recvMessage(varName, this->srcId_);
             numberElemRcv_++;
-
-//            std::cout << id_ << " Received data: " << rcv << " out of " << numberBlocksToReceive_ << std::endl;
-
             this->addResult(CommMatrixBlock<MatrixType>::deserialize(iss));
         }
     }
@@ -244,34 +165,26 @@ public:
 // Send task to send data to a destination id to the communicator
 template<class MatrixType>
 class CommSendTask : public hh::AbstractTask<void, CommMatrixBlock<MatrixType>> {
-    int id_;
-    std::shared_ptr<Communicator> communicator_;
 public:
-    explicit CommSendTask(int id, std::shared_ptr<Communicator> communicator) :
-            hh::AbstractTask<void, CommMatrixBlock<MatrixType>>("Comm Sender task"), id_(id),
-            communicator_(communicator) {}
+    explicit CommSendTask() :
+            hh::AbstractTask<void, CommMatrixBlock<MatrixType>>("Comm Sender task") {}
     ~CommSendTask() override = default;
 
     void execute(std::shared_ptr<CommMatrixBlock<MatrixType>> block) override {
         std::ostringstream oss;
-
         block->serialize(oss);
-
-        assert(this->id_ == block->src());
-
-        communicator_->sendMessage(oss, this->id_, block->dest());
+        comm::Communicator::sendMessage(oss, block->dest());
     }
-
 };
 
 // Task to print the final result
 template<class MatrixType>
 class PrintBlockTask : public hh::AbstractTask<void, CommMatrixBlock<MatrixType>> {
-    int id_;
+    int nodeId_;
 public:
-    PrintBlockTask(int id) : hh::AbstractTask<void, CommMatrixBlock<MatrixType>>("PrintTask"), id_(id) {}
+    PrintBlockTask() : hh::AbstractTask<void, CommMatrixBlock<MatrixType>>("PrintTask"), nodeId_(comm::getMpiNodeId()) {}
     void execute([[maybe_unused]]std::shared_ptr<CommMatrixBlock<MatrixType>> ptr) override {
-        std::cout << this->id_ << " print" << std::endl;
+        std::cout << this->nodeId_ << " print" << std::endl;
         std::cout << *ptr << std::endl;
     }
 };
@@ -299,29 +212,15 @@ public:
 
 // Routine that decomposes the data to setup source and destinations for each data
 template<class MatrixType>
-std::vector<std::shared_ptr<CommMatrixBlock<MatrixType>>> decomposeData(int numRows, int numCols, int numNodes, int height, int width) {
+std::vector<std::shared_ptr<CommMatrixBlock<MatrixType>>> decomposeData(int numRows, int numCols, int height, int width) {
     std::vector<std::shared_ptr<CommMatrixBlock<MatrixType>>> retData;
 
-    int destNode = 0;
-    int srcNode = numNodes-1;
-
-    for (int r = 0; r < numRows; ++r) {
-        for (int c = 0; c < numCols; ++c) {
-
-            auto block = std::make_shared<CommMatrixBlock<MatrixType>>(srcNode, destNode, r, c, height, width);
-            retData.push_back(block);
-
-            srcNode--;
-            if (srcNode < 0) {
-                srcNode = numNodes-1;
-            }
-
-            destNode++;
-
-            if (destNode == numNodes) {
-                destNode = 0;
-            }
-        }
+    int srcNode = comm::getMpiNodeId();
+    int destNode = numCols-srcNode-1;
+    for (int r = 0, c = srcNode; r < numRows; ++r) {
+        auto varName = "block"+std::to_string(r)+std::to_string(c);
+        auto block = std::make_shared<CommMatrixBlock<MatrixType>>(varName, srcNode, destNode, r, c, height, width);
+        retData.push_back(block);
     }
 
     return retData;
@@ -329,20 +228,24 @@ std::vector<std::shared_ptr<CommMatrixBlock<MatrixType>>> decomposeData(int numR
 
 // Creates the hedgehog graphs with send/receive communication across multiple graphs
 template<class MatrixType>
-std::shared_ptr<hh::Graph<void, CommMatrixBlock<MatrixType>>> createGraph(int id, std::shared_ptr<Communicator> communicator, size_t numDataReceived) {
+std::shared_ptr<hh::Graph<void, CommMatrixBlock<MatrixType>>> createGraph(size_t numDataReceived) {
+    int rank = comm::getMpiNodeId();
+    int numNodes = comm::getMpiNumNodes();
+    int srcNode = numNodes-rank-1;
+
     auto graph = std::make_shared<hh::Graph<void, CommMatrixBlock<MatrixType>>>("NodeGraph");
 
-    auto localState = std::make_shared<LocalMatrixState<MatrixType>>(id);
-    auto remoteState = std::make_shared<RemoteMatrixState<MatrixType>>(id);
+    auto localState = std::make_shared<LocalMatrixState<MatrixType>>();
+    auto remoteState = std::make_shared<RemoteMatrixState<MatrixType>>();
 
     auto localStateManager = std::make_shared<hh::StateManager<CommMatrixBlock<MatrixType>, CommMatrixBlock<MatrixType>>>(localState);
     auto remoteStateManager = std::make_shared<hh::StateManager<CommMatrixBlock<MatrixType>, CommMatrixBlock<MatrixType>>>(remoteState);
 
     auto accumulateTask = std::make_shared<AddTask<MatrixType>>(1, 42);
-    auto printTask = std::make_shared<PrintBlockTask<MatrixType>>(id);
+    auto printTask = std::make_shared<PrintBlockTask<MatrixType>>();
 
-    auto receiverTask = std::make_shared<CommReceiveTask<MatrixType>>(id, communicator, numDataReceived);
-    auto sendTask = std::make_shared<CommSendTask<MatrixType>>(id, communicator);
+    auto receiverTask = std::make_shared<CommReceiveTask<MatrixType>>(srcNode, numDataReceived);
+    auto sendTask = std::make_shared<CommSendTask<MatrixType>>();
 
     graph->input(localStateManager);
     graph->input(remoteStateManager);
@@ -358,54 +261,42 @@ std::shared_ptr<hh::Graph<void, CommMatrixBlock<MatrixType>>> createGraph(int id
 
 int main(int argc, char **argv) {
     using MatrixType = float;
+    comm::MPI_GlobalLockGuard mpiGlobalLockGuard(&argc, &argv);
 
-    int numNodes = 5;
-    int gridWidth = 5;
+    int gridWidth = comm::getMpiNumNodes();
     int gridHeight = 5;
     int blockWidth = 2;
     int blockHeight = 2;
 
-    auto communicator = std::make_shared<Communicator>();
-    communicator->initComm(numNodes);
-
-    std::vector<std::shared_ptr<hh::Graph<void, CommMatrixBlock<MatrixType>>>> graphs(numNodes);
+    std::shared_ptr<hh::Graph<void, CommMatrixBlock<MatrixType>>> graph;
 
     // Decompose data to have source and destinations
-    auto dataVector = decomposeData<MatrixType>(gridHeight, gridWidth, numNodes, blockHeight, blockWidth);
+    auto dataVector = decomposeData<MatrixType>(gridHeight, gridWidth, blockHeight, blockWidth);
 
     // Calculate how many data would be received for each node (used to terminate)
-    std::vector<int> numDataReceived(numNodes);
+    int numDataReceived = 0;
     for (auto data : dataVector) {
         if (data->src() != data->dest()) {
-            numDataReceived[data->dest()]++;
+            numDataReceived++;
         }
     }
 
     // Create the hedgehog graphs
-    for (int nodeId = 0; nodeId < numNodes; ++nodeId) {
-        graphs[nodeId] = createGraph<MatrixType>(nodeId, communicator, numDataReceived[nodeId]);
-    }
+    graph = createGraph<MatrixType>(numDataReceived);
 
     // Execute the hedgehog graphs
-    for (auto graph : graphs) {
-        graph->executeGraph();
-    }
+    graph->executeGraph();
 
     // Push data into the hedgehog graphs (index into graphs vector represents node, so only send to srcNode)
     for (auto data : dataVector) {
-        int srcNode = data->src();
-        graphs[srcNode]->pushData(data);
+        graph->pushData(data);
     }
 
     // Indicate each graph is done receiving data from main
-    for (auto graph : graphs) {
-        graph->finishPushingData();
-    }
+    graph->finishPushingData();
 
     // Wait for graphs to finish executing
-    for (auto graph : graphs) {
-        graph->waitForTermination();
-    }
+    graph->waitForTermination();
 
     std::cout << "Finished" << std::endl;
 
